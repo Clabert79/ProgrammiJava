8054b041e8d45a6814b27b59931209756167de43
clabert79prova <clabert79git@gmail.com>
2017-05-27 01:07:08 -0700

    Modify Prova1

--- Prova1.java
+++ Prova1.java
@@ -1,9 +1,9 @@
 public class Prova1{
 	int x,y;
-	public static void main (String args[]){
+			public static void main (String args[]){
 		// prova
 		System.out.print("Hello!!");
-		System.out.println("Hello");
-		System.out.println("Word");
+		System.out.println("Hello1111");
+		System.out.println("Word1111");
 	}
 }
