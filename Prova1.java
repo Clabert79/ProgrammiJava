ce0a00e9bb557c4cea2eb212af0c0820242d5d67
Claudio Berolotto <clabert79Git@gmail.com>
2017-05-28 09:01:29 +0200

    modify from mari mac

--- Prova1.java
+++ Prova1.java
@@ -1,8 +1,8 @@
 public class Prova1{
 	int x,y;
-			public static void main (String args[]){
+		public static void main (String args[]){
 		// prova
-		System.out.print("Hello!!");
+		System.out.print("Mac !!!!");
 		System.out.println("Hello1111");
 		System.out.println("Word1111");
 	}
